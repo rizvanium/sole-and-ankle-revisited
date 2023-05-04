2f8ccef7a6e935989f9fd63625be6117d97ba42e
Rizvan <rizvan.chalilovas@proton.me>
2023-05-04 11:20:29 +0300

    add breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+const BREAKPOINTS = {
+  phone: "37.5",
+  tablet: "59.375",
+  laptop: "81.25"
+}
+
+export const QUERIES = { 
+  "laptopAndSmaller": `(max-width: ${BREAKPOINTS.laptop}rem)`,
+  "tabletAndSmaller": `(max-width: ${BREAKPOINTS.tablet}rem)`,
+  "phoneAndSmaller": `(max-width: ${BREAKPOINTS.phone}rem)`,
+}
